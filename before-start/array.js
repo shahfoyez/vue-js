89d165ee7bb4c6dec2f649269574326875cc47ef
shahfoyez <shahfoyez7@gmail.com>
2024-02-27 15:08:54 +0600

    new

---
+++ before-start/array.js
@@ -0,0 +1,15 @@
+// destructuring
+var countries = ['Bangladesh', 'Morocco', 'Russia'];
+// map
+
+// set
+<div class="foy-hero">
+    <video class="hero-video" preload="" playsInline="" muted="" autoPlay="" loop="">
+        <source src="https://downloads.creativelive.com/homepage/hero_video_v2.mp4" type="video/mp4"/>
+    </video>
+    <div class="hero-content">
+        <h1>Do what you love.</h1>
+        <h4>Learn the skills you need to live your dreams in career, hobby, and life. </h4>
+        <button class="foy-start-btn"></button>
+    </div>
+</div>
---
+++ before-start/function.js
@@ -0,0 +1,17 @@
+// function hello(...x){
+//     for(var n in x){
+//        console.log(n);
+//     }
+// }
+// hello(1,2,3,4,5,6,7,8,9,10,11);
+
+var num = function(x){
+    return x;
+}
+var value = num(5);
+console.log(value);
+
+var y = ()=>{
+    console.log("Hello");
+}
+ y();
